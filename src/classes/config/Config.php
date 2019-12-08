2f6cc3344e9d4d6b2e015a13f9581938e4079af9
trapeznikova-evgenija <trapeznikova.evg@yandex.ru>
2019-12-08 17:20:40 +0300

    изменен файл конфига

--- src/classes/config/Config.php
+++ src/classes/config/Config.php
@@ -2,11 +2,13 @@
 
 class Config
 {
-    const MYSQL_HOST = 'localhost';
+    const MYSQL_HOST = 'mysql';
     const MYSQL_USERNAME = 'root';
     const MYSQL_PASSWORD = '1234';
     const MYSQL_DATABASE = 'load_test';
     const MYSQL_PORT = '3306';
+
+    const DB_TABLE_VERSIONS = 'versions';
 
     const APY_KEY = 'A.7a90c9f8293c2f09d0ea68c78e19c1f6';
 
