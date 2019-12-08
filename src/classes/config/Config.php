0da915736c01d05fc0ced383d7e13f1d69726ea1
nastyaaa19980 <nastyaaa19980@gmail.com>
2019-12-09 00:08:33 +0300

    Поправлен конфиг

--- src/classes/config/Config.php
+++ src/classes/config/Config.php
@@ -10,7 +10,7 @@ class Config
     const MYSQL_HOST = 'localhost';
     const MYSQL_USERNAME = 'root';
     const MYSQL_PASSWORD = '1234';
-    const MYSQL_DATABASE = 'load_test';
+    const MYSQL_DATABASE = 'pageloadtest';
     const MYSQL_PORT = '3306';
 
     const DB_TABLE_VERSIONS = 'versions';
